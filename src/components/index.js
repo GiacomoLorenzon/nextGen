d4812e6dba735249fc9f136485e2e15396825f22
Giacomo Lorenzon <giacomo.lorenzon@mail.polimi.it>
2025-02-11 21:39:18 +0100

    Updates

--- src/components/index.js
+++
@@ -1,6 +0,0 @@
-export { default as Landing } from "./Landing";
-export { default as Info } from "./Info";
-export { default as Plans } from "./Plans";
-export { default as Reviews } from "./Reviews";
-export { default as Form } from "./Form";
-export { default as Footer } from "./Footer";
